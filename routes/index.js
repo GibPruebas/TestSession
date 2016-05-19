e2cce2a64aa1e5d125b3c543037ee9a77bcb266b
Hector Rosales <nestory80@hotmail.com>
2016-05-18 23:35:57 -0500

    Commit message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+function isAuthenticated (req,res,next){
+  //if (!req.session.user_id){
+  if (!req.session.username){
+    res.render('not_authorized');
+    console.log(req.session.username);
+  } else {
+    next();
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index',{title:'Express'});
 });
 
 router.get('/login', function(req, res, next) {
@@ -11,38 +21,38 @@ router.get('/login', function(req, res, next) {
 });
 
 
+
 router.post('/login', function(req, res, next) {
-    req.session.mail=req.body.mail;
+    req.session.username=req.body.username;
     
-    var pagina='<!doctype html><html><head></head><body>'+
-               '<p>Se creo la variable de sesión</p>'+
-               '<p>Puede ingresar al panel de control:</p>'+
-               '<a href="/panel">Ingresar</a><br>'+
-               '</body></html>';
-    res.send(pagina); 
+    //var pagina='<!doctype html><html><head></head><body>'+
+               //'<p>Acceso valido</p>'+
+               //'<p>Puede ingresar al panel de control:</p>'+
+               //'<a href="/dashboard">Ingresar</a><br>'+
+               //'</body></html>';
+    //res.send(pagina); 
+    //res.render('dashboard'); 
+
+    //res.render('dashboard'); 
+    res.redirect('/dashboard');   
+
 });
 
 
-router.get('/panel', function(req, res, next) {
-    if (req.session.mail) {
-      var pagina='<!doctype html><html><head></head><body>'+
-               '<p>Bienvenido</p>'+
-               req.session.mail+
-               '<br><a href="/logout">Logout</a></body></html>';
-      res.send(pagina);
-    } else {
-      var pagina='<!doctype html><html><head></head><body>'+
-               '<p>No tiene permitido ingresar sin login</p>'+
-               '<br><a href="/">Retornar</a></body></html>';
-      res.send(pagina);        
-    }
-});
+router.get('/dashboard',isAuthenticated, function(req, res) {
+    //if (req.session.username) {
+      //res.render('dashboard');    
+    //} else {
+      //res.render('not_authorized');
+    //}
 
+    res.render('dashboard');
+});
 
 router.get('/logout', function(req, res, next) {
       req.session.destroy();
       var pagina='<!doctype html><html><head></head><body>'+
-               '<br><a href="/">Retornar</a></body></html>';
+               '<br><a href="/login">Retornar</a></body></html>';
       res.send(pagina);
 });
 
